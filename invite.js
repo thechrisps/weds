efea07f88ffbe867f21ca827f12d9521fa2a4ba7
Chris P <Chris P>
2017-06-14 21:58:38 +0100

    Test connection strin

--- invite.js
+++ invite.js
@@ -1,7 +1,9 @@
 module.exports = function(app){
 
+    var connection = process.env.MYSQLCONNSTR_localdb.match(/^.*Data Source=(.+?);.*$/gm)
+
     app.get('/invite', function(req, res){
-        res.send("Test");
+        res.send("Test:"+connection);
     });
 
     //other routes..
