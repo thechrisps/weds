05b47758c6a42cec45d4562c0f3128616aa6d66a
Chris P <Chris P>
2017-06-18 21:06:24 +0100

    Database definition code

--- invite.js
+++ invite.js
@@ -1,9 +1,9 @@
 module.exports = function(app){
 
-    var connection = process.env.MYSQLCONNSTR_localdb.match(/^.*Data Source=(.+?);.*$/gm)
+
 
     app.get('/invite', function(req, res){
-        res.send("Test:"+connection);
+        res.send("Test:"+util.getDatabaseDefinition().stringify());
     });
 
     //other routes..
