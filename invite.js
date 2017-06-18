487ffff4c1875a1b137b5b585983f9bf71fbe45f
Chris P <Chris P>
2017-06-18 22:34:37 +0100

    Database definition code - fixed bug 3

--- invite.js
+++ invite.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
     app.get('/invite', function(req, res){
-        res.send("Test:"+utils.getDatabaseDefinition().toString());
+      res.send("Test");
+        //res.send("Test:"+utils.getDatabaseDefinition().toString());
     });
 
     //other routes..
