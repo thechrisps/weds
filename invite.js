0bab4f03e5ee1a7315b22ebbffee87b536a54346
Chris P <Chris P>
2017-06-19 21:56:04 +0100

    Trying out URL params code

--- invite.js
+++ invite.js
@@ -1,8 +1,10 @@
 var utils = require("./utils");
 
 module.exports = function(app){
-    app.get('/invite', function(req, res){
+    app.get('/invite/:code', function(req, res){
+      var inviteCode = req.params.code;
       res.send("Test:"+utils.getDatabaseDefinition().toString());
+      res.send("Invite: "+inviteCode);
     });
 
     //other routes..
