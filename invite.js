99cede317649e8a3f099388e0835213d6d36274c
Chris Porteous <chrisps@gmail.com>
2017-08-01 21:18:11 +0100

    Adding invite page and register page and starting glue logic for EJS

--- invite.js
+++ invite.js
@@ -1,10 +1,11 @@
 var utils = require("./utils");
 
-module.exports = function(app){
+module.exports = function (app) {
     app.get('/invite/:code', function(req, res){
       var inviteCode = req.params.code;
       if (isValidInvite(inviteCode)) {
-        utils.dispatchJsonResponse(res, {"status": "ok", "response": {"valid": "1"}});
+        res.render("register", { "friendlyName": "Bob" });
+       //utils.dispatchJsonResponse(res, {"status": "ok", "response": {"valid": "1"}});
       } else {
         utils.dispatchJsonResponse(res, {"status": "ok", "response": {"valid": "0"}});
       }
