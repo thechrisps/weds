f33970e5d36f3512057d985852d7709a3277c8bc
Chris Porteous <chrisps@gmail.com>
2017-08-04 21:02:57 +0100

    Redirect from invite should work

--- views/js/candh.js
+++ views/js/candh.js
@@ -6,20 +6,19 @@ $('#btnInviteSubmit').click(function (event) {
     enteredCode = enteredCode.toUpperCase();
 
     $.ajax({
-        url: '/validateinvite/'+enteredCode,
+        url: '/validateinvite/' + enteredCode,
         error: cbInviteValidateError,
-        success: cbInviteValidateSuccess,
+        success: function (data) {
+            if (data.response.valid === "1") {
+                window.location.replace("/invite/" + enteredCode);
+            } else {
+                $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, your code doesn't appear to be valid. Please check and try again. Still having problems? Please drop us a message on Facebook or by phone. Thanks!");
+            }
+        },
         type: 'GET'
     });
 });
 
-var cbInviteValidateSuccess = function (data, textStatus, xhr) {
-    if (data.response.valid === "1") {
-        alert("redirect");
-    } else {
-        $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, your code doesn't appear to be valid. Please check and try again. Still having problems? Please drop us a message on Facebook or by phone. Thanks!&nbsp;<img src='img/exclaimation.png' alt='' />");
-    }
-}
 var cbInviteValidateError = function (error, xhr) {
-    $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, an error has occurred processing your request. Please drop us a message on Facebook or by phone. Thanks!&nbsp;<img src='img/exclaimation.png' alt='' />");
+    $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, an error has occurred processing your request. Please drop us a message on Facebook or by phone. Thanks!");
 }
