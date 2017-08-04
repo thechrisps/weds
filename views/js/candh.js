7c98c364496e08aeca2c61d6b608e454c165518c
Chris Porteous <chrisps@gmail.com>
2017-08-04 20:52:59 +0100

    Extracting out callbacks

--- views/js/candh.js
+++ views/js/candh.js
@@ -7,21 +7,20 @@ $('#btnInviteSubmit').click(function (event) {
 
     $.ajax({
         url: '/validateinvite/'+enteredCode,
-        data: {
-            format: 'json'
-        },
-        error: function () {
-            $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, an error has occurred processing your request. Please drop us a message on Facebook or by phone. Thanks!&nbsp;<img src='img/exclaimation.png' alt='' />");
-        },
+        error: cbInviteValidateError,
         dataType: 'jsonp',
-        success: function (data) {
-            if (data.response.valid === "1")
-            {
-                alert("redirect");
-            } else {
-                $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, your code doesn't appear to be valid. Please check and try again. Still having problems? Please drop us a message on Facebook or by phone. Thanks!&nbsp;<img src='img/exclaimation.png' alt='' />");
-            }
-        },
+        success: cbInviteValidateSuccess,
         type: 'GET'
     });
 });
+
+var cbInviteValidateSuccess = function (data, textStatus, xhr) {
+    if (data.response.valid === "1") {
+        alert("redirect");
+    } else {
+        $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, your code doesn't appear to be valid. Please check and try again. Still having problems? Please drop us a message on Facebook or by phone. Thanks!&nbsp;<img src='img/exclaimation.png' alt='' />");
+    }
+}
+var cbInviteValidateError = function (error, xhr) {
+    $('#inviteMessage').html("<img src='img/exclaimation.png' alt='' /><br />Sorry, an error has occurred processing your request. Please drop us a message on Facebook or by phone. Thanks!&nbsp;<img src='img/exclaimation.png' alt='' />");
+}
