455dfa8d1ee1b0404bc1337f513bb6c807308d53
Chris Porteous <chrisps@gmail.com>
2017-08-04 20:01:54 +0100

    Splitting up EJS better

--- invite.js
+++ invite.js
@@ -1,14 +1,26 @@
 var utils = require("./utils");
 
 module.exports = function (app) {
+    app.get('/invite/', function (req, res) {
+        res.render("invite");
+    });
+
     app.get('/invite/:code', function(req, res){
       var inviteCode = req.params.code;
       if (isValidInvite(inviteCode)) {
         res.render("register", { "friendlyName": "Bob" });
-       //utils.dispatchJsonResponse(res, {"status": "ok", "response": {"valid": "1"}});
       } else {
         utils.dispatchJsonResponse(res, {"status": "ok", "response": {"valid": "0"}});
       }
+    });
+
+    app.get('/validateinvite/:code', function (req, res) {
+        var inviteCode = req.params.code;
+        if (isValidInvite(inviteCode)) {
+            utils.dispatchJsonResponse(res, {"status": "ok", "response": {"valid": "1"}});
+        } else {
+            utils.dispatchJsonResponse(res, { "status": "ok", "response": { "valid": "0" } });
+        }
     });
 
     //other routes..
